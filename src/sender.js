21f63098f9c5cdc29823c4e30f12d6a16d63b86c
peter.okwara <peter.okwara@outlook.com>
2019-10-06 20:37:50 +0300

    Transmit sensor data to the MAM

--- src/sender.js
+++ src/sender.js
@@ -1,9 +1,40 @@
 // importing the mam client library
 const Mam = require("@iota/mam");
 
+// import the converter module
+const Converter = require("@iota/converter");
+
 // define the entry point into the tange
 const provider = "https://nodes.devnet.iota.org:443";
 
 // initialize the mam object
 let state = Mam.init(provider);
-console.log(state);
+//console.log(state);
+
+// define the function to send the message
+const publish = async function(packet) {
+  const trytes = Converter.asciiToTrytes(JSON.stringify(packet));
+
+  const message = Mam.create(state, trytes);
+  state = message.state;
+  //console.log(state);
+
+  await Mam.attach(message.payload, message.address);
+  console.log(message.root);
+  return message.root;
+};
+
+// generate random, JSON formatted data
+const sensorData = () => {
+  return JSON.stringify({
+    time: new Date(),
+    data: Math.floor(Math.random() * 100 + 1)
+  });
+};
+
+// publish sensor data
+setInterval(() => {
+    publish(sensorData())
+}, 10000);
+
+//publish("**ping**");
